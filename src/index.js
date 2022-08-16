19585ba4cf2bedb47d158b1bd9ecd61e1c8d0315
grigorchik <grigorchik.alexnadr@gmail.com>
2022-08-16 20:58:18 +0300

    day 1

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.scss';
 import App from './App';
-
+import './index.scss';
+import 'macro-css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
